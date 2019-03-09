e880d61e9341a577e4766021965f0380af04100d
codeAlfred <munozrodas.luisalfredo@gmail.com>
2019-03-09 11:40:43 -0500

    gulfile configurado

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const pug = require('gulp-pug');
+const browserSync = require('browser-sync');
+
+
+gulp.task('default',['css'], function(){
+    browserSync.init({
+       serve: './app/' 
+    });
+    gulp.watch('./dev/sass/**/*.scss', ['css']);
+    gulp.watch('./dev/**/*.pug', ['html']);
+    gulp.watch('./app/*.html').on('change',browserSync.reload);
+    
+    
+});
+
+gulp.task('css', function(){
+   gulp.src('./dev/sass/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./app/css/'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function(){
+    gulp.src('./dev/pug/*.pug')
+    .pipe(pug({
+        pretty: true
+    }))
+    .pipe(gulp.dest('./app/'));
+});
